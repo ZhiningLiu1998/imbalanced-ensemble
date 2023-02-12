260ce06412f51c3b3b391854da2d1b06ad58e548
ZhiningLiu1998 <zhining.liu@outlook.com>
2023-02-12 02:58:00 -0600

    FIX ResampleBagging's sample_weight support

--- imbalanced_ensemble/ensemble/_bagging.py
+++ imbalanced_ensemble/ensemble/_bagging.py
@@ -125,15 +125,10 @@ def _parallel_build_estimators(n_estimators, ensemble, X, y, sample_weight,
                                                       max_samples)
 
         # Draw samples, using sample weights, and then fit
-        if support_sample_weight:
-            if sample_weight is None:
-                curr_sample_weight = np.ones((n_samples,))
-            else:
-                curr_sample_weight = sample_weight.copy()
-
+        if support_sample_weight and sample_weight is not None:
+            curr_sample_weight = sample_weight.copy()
             estimator.fit((X[indices])[:, features], y[indices], 
                 sample_weight=curr_sample_weight[indices])
-
         else:
             estimator.fit((X[indices])[:, features], y[indices])
         
